2c9718c0ed54abdd8704ac75573790d566b30130
hakimghaziali@gmail.com <hakimghaziali@gmail.com>
2022-07-26 11:48:07 +0430

    add models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+
+STATUS= (
+    (0, 'Draft'),
+    (1,'Publish')
+)
+
+
+class Post(models.Model):
+
+
+    title= models.CharField(max_length=40)
+    content = models.TextField()
+    author = models.ForeignKey(User , on_delete=models.CASCADE , related_name='blog_posts')
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    status= models.IntegerField(choices=STATUS , default=0)
+
+    def __str__(self):
+
+        return self.title
+
